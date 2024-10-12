6652c40d00d79a76a754b526e44c83bf946e6f08
Abdulmujeeb <abdulroheemabdulmujeeb@gmail.com>
2024-10-11 23:05:38 -0700

    first commit

---
+++ src/components/ChannelCard.jsx
@@ -0,0 +1,16 @@
+import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+import {
+  demoThumbnailUrl,
+  demoVideoUrl,
+  demoVideoTitle,
+  demoChannelTitle,
+  demoChannelUrl,
+} from "../utility/constant";
+import { Link } from "react-router-dom";
+
+const ChannelCard = () => {
+  return <div>Someth</div>;
+};
+
+export default ChannelCard;
---
+++ src/components/Videos.jsx
@@ -0,0 +1,17 @@
+import { Box, Stack } from "@mui/material";
+import { VideoCard, ChannelCard } from "./";
+
+const Videos = ({ videos }) => {
+  return (
+    <Stack direction={"row"} flexWrap={"wrap"} justifyContent={"start"} gap={2}>
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.ChannelId && <ChannelCard ChannelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
+};
+
+export default Videos;
